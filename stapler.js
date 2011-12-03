723993bd8c826347ac1ab84b0846af21caaae618
Bernardo B. Marques <bernardo.fire@gmail.com>
2011-12-03 02:20:50 -0200

    fixes and changes

---
+++ stapler.js
@@ -0,0 +1,87 @@
+function addRepo(repoInfo){
+
+    var repoOwner = repoInfo.replace(new RegExp("/.*"),'');
+
+    var repoName = repoInfo.replace(new RegExp(".*/"), '');
+
+    var repos = JSON.parse(localStorage.getItem('repos')) || {};
+
+    // TODO: This way I can't have more than one repo with the same name, fix it!
+    repos[repoName] = repoOwner;
+
+    // Add to localStorage
+    localStorage.setItem('repos', JSON.stringify(repos));
+}
+
+
+function addClipButton(){
+
+    var repoURL = $('[rel=alternate]')[0].getAttribute('href')
+                    .replace('https://github.com/', '')
+                    .replace('/commits/master.atom', '');
+
+    var button = $('<li class="js-toggler-container">')
+        .append('<a href="#" class="minibutton mousedown">');
+        button.children('a').text('Clip!')
+        .bind('click',addRepo(repoURL));
+
+    $('.pagehead-actions').append(button)
+
+}
+
+
+function createClipPanel(){
+
+    var Panel = $('<div class="repos" id="clipped_repos">').hide()
+
+    Panel.append('<div class="top-bar">')
+    Panel.children('.top-bar').append('<h2> Clipped Repositories </h2>')
+
+    Panel.append('<ul class="repo_list" id="clipped_repos_listing">')
+
+    Panel.append('<div class="bottom-bar">');
+    Panel.children('.bottom-bar').append('<a href="#" class="show-more" id="inline_clipped_repos">')
+
+    $('div[id=dashboard]').prepend(Panel);
+}
+
+
+function fillPanel (){
+
+    var getRepos = JSON.parse(localStorage.getItem('repos'))
+
+    for (key in getRepos){
+
+        var repoName = key;
+        var repoOwner = getRepos[key];
+
+        var li = $('<li class="public source">').append('<a>');
+        li.children('a').attr({'href': '/' + repoOwner + '/' + repoName})
+            .append('<span class="owner">')
+            .append('/')
+            .append('<span class="repo">');
+
+        li.children('a').children('.owner').text(repoOwner);
+        li.children('a').children('.repo').text(repoName);
+
+        $('#clipped_repos_listing').append(li);
+
+
+    }
+}
+
+function handleURLS(){
+
+    var url = window.location.href
+
+    if (url == "https://github.com/" || url == "https://github.com/dashboard/yours"){
+        createClipPanel();
+        fillPanel();
+        $('#clipped_repos').show();
+
+    } else if ($('.watch-button').length > 0){
+        addClipButton();
+    }
+}
+
+handleURLS()
